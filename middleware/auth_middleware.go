997002dc976bc31a1c095af298f39bc30278137e
Arya Yudha Saputra <100254907+AryaYS@users.noreply.github.com>
2024-02-29 15:54:03 +0700

    up for Middleware

---
+++ middleware/auth_middleware.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"apilogin/model/response"
+	"encoding/json"
+	"net/http"
+)
+
+type AuthMiddleware struct {
+	Handler http.Handler
+}
+
+func NewAuthMiddleware(handler http.Handler) *AuthMiddleware {
+	return &AuthMiddleware{Handler: handler}
+}
+
+func (middleware AuthMiddleware) ServeHTTP(wr http.ResponseWriter, req *http.Request) {
+	if req.Header.Get("X-API-Key") == "RAHASIA" {
+		middleware.Handler.ServeHTTP(wr, req)
+	} else {
+		wr.Header().Set("Content-Type", "application/json")
+		wr.WriteHeader(http.StatusUnauthorized)
+
+		webResponse := response.WebResponse{
+			Code:   http.StatusNotFound,
+			Status: "UNAUTHORIZED",
+		}
+		wr.Header().Add("Content-Type", "application/json")
+		encoder := json.NewEncoder(wr)
+		err := encoder.Encode(webResponse)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
